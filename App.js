cca97e9cb3cb08e56733af0eb08900a4bf321ad5
Max <mrashes2@gmail.com>
2018-07-02 22:38:09 -0500

    rudimentary monitoring implementation

--- App.js
+++ App.js
@@ -1,23 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, WebView } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      url: 'https://en.wikipedia.org/wiki/Albert_Einstein',
+      path: [],
+      steps: 0,
+    }
+  }
+
+  _onNavigationStateChange(webViewState){
+    const currLoc = webViewState.url.split('/')[4]
+    const pastLoc = this.state.path[this.state.path.length-1]
+    console.log(webViewState)
+    if (pastLoc !== currLoc && webViewState.loading == false) {
+      // console.log('hit this')
+      const newArray = this.state.path.concat(currLoc)
+      this.setState(() => {
+        return { path: newArray };
+      });
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    <View style={styles.container}>
+      <WebView
+        ref="webview"
+        source={{uri:this.state.url}}
+        onNavigationStateChange={this._onNavigationStateChange.bind(this)}
+        javaScriptEnabled = {true}
+        style={styles.webView}
+      />
+      <Text>{this.state.path.join(", ")}</Text>
+    </View>
+    )
   }
 }
 
 const styles = StyleSheet.create({
+  webView: {
+    marginTop: 20, 
+    flex: 1
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
